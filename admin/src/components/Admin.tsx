f0b7704394edcc981fff2131bc79c20a225a1f31
Leo Nistor <leo.nistor@gmail.com>
2026-05-01 20:13:11 +0300

    customers

--- admin/src/components/Admin.tsx
+++ admin/src/components/Admin.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@payloadcms/ui'
 import type { CollectionSlug, Payload } from 'payload'
+import './Admin.scss'
 
 export async function Overview({ payload }: { payload: Payload }) {
   const getPublishedUnpublished = async (collection: CollectionSlug) => {
@@ -14,11 +15,43 @@ export async function Overview({ payload }: { payload: Payload }) {
   const partners_published = partners_counts.published
   const partners_unpublished = partners_counts.unpublished
 
+  const customers_counts = await getPublishedUnpublished('customers')
+  const customers_published = customers_counts.published
+  const customers_unpublished = customers_counts.unpublished
+
   return (
-    <>
-      <Button buttonStyle="pill">
-        {partners_published.totalDocs} + {partners_unpublished.totalDocs} Partners
-      </Button>
-    </>
+    <div className="overview">
+      <OverviewButton
+        name="partners"
+        count_published={partners_published.totalDocs}
+        count_unpublished={partners_unpublished.totalDocs}
+      />
+      <OverviewButton
+        name="customers"
+        count_published={customers_published.totalDocs}
+        count_unpublished={customers_unpublished.totalDocs}
+      />
+    </div>
   )
+}
+
+function OverviewButton({
+  name,
+  count_published,
+  count_unpublished,
+}: {
+  name: string
+  count_published: number
+  count_unpublished: number
+}) {
+  return (
+    <Button el="link" to="/collections" buttonStyle="pill">
+      {capitalizeFirstLetter(name)}: {count_published}
+      {count_unpublished ? ` + ${count_unpublished}` : ''}
+    </Button>
+  )
+}
+
+function capitalizeFirstLetter(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1)
 }
