0ad5c2c6f964af9543cb524d1a2ea82594003e55
Leo Nistor <leo.nistor@gmail.com>
2026-05-03 11:22:58 +0300

    init export manually

--- admin/seed/export.ts
+++ admin/seed/export.ts
@@ -1,1 +1,11 @@
 // TODO: also use plugin
+import config from '@payload-config'
+import { getPayload } from 'payload'
+
+const payload = await getPayload({ config })
+
+const all_collections = payload.config.collections.filter((c) => !c.admin.hidden).map((c) => c.slug)
+
+console.debug(all_collections)
+
+process.exit(0)
