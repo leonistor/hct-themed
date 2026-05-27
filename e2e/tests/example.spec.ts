0007537d35402daaba1e4122e40cb911b0c2d02f
Leo Nistor <leo.nistor@gmail.com>
2026-05-27 18:14:30 +0300

    try biome

--- e2e/tests/example.spec.ts
+++ e2e/tests/example.spec.ts
@@ -1,50 +1,47 @@
-import { test } from '../fixtures'
-import { expect } from '@playwright/test';
-
+import { expect } from "@playwright/test";
 import menu_ro from "../../web/src/config/menu.ro.json" with { type: "json" };
+import { test } from "../fixtures";
 
-test('has title', async ({ page }) => {
-    await page.goto('/');
-
+test("has title", async ({ page }) => {
+	await page.goto("/");
 });
 
-test('visit homepage', async ({ page }) => {
-  await page.goto('/');
-  await expect(page).toHaveTitle(/Trading/);
+test("visit homepage", async ({ page }) => {
+	await page.goto("/");
+	await expect(page).toHaveTitle(/Trading/);
 });
 
-
-test('visit menu items', async ({ page }) => {
-  await page.goto('/');
-  for (let url of getMenuItems()) {
-    test.slow()
-    await page.goto(url);
-  }
+test("visit menu items", async ({ page }) => {
+	await page.goto("/");
+	for (const url of getMenuItems()) {
+		test.slow();
+		await page.goto(url);
+	}
 });
 
 function getMenuItems() {
-  let result = [];
-  for (let item of menu_ro.headerPrimary) {
-    if (item.children) {
-      for (let child of item.children) {
-        if (child.children) {
-          for (let grandchild of child.children) {
-            result.push(grandchild.url);
-          }
-        } else {
-          result.push(child.url);
-        }
-      }
-    } else if (item.menus) {
-      for (let menu of item.menus) {
-        for (let submenu of menu.children) {
-          result.push(submenu.url);
-        }
-      }
-    } else {
-      result.push(item.url);
-    }
-  }
+	const result = [];
+	for (const item of menu_ro.headerPrimary) {
+		if (item.children) {
+			for (const child of item.children) {
+				if (child.children) {
+					for (const grandchild of child.children) {
+						result.push(grandchild.url);
+					}
+				} else {
+					result.push(child.url);
+				}
+			}
+		} else if (item.menus) {
+			for (const menu of item.menus) {
+				for (const submenu of menu.children) {
+					result.push(submenu.url);
+				}
+			}
+		} else {
+			result.push(item.url);
+		}
+	}
 
-  return result;
+	return result;
 }
