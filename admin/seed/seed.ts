2e702843ef51154c81610a6edd68eb30b168e7b2
Leo Nistor <leo.nistor@gmail.com>
2026-08-18 09:48:09 +0300

    Restore customers with delete all then `bun --bun seed/seed.ts` in admin

--- admin/seed/seed.ts
+++ admin/seed/seed.ts
@@ -1,13 +1,13 @@
-import { seed_partners } from './partners'
+// import { seed_partners } from './partners'
 import { seed_customers } from './customers'
-import { seed_categories } from './categories'
-import { seed_materials } from './materials'
-import { seed_folders } from './folders'
-
-await seed_partners()
-await seed_folders()
+// import { seed_categories } from './categories'
+// import { seed_materials } from './materials'
+// import { seed_folders } from './folders'
+//
+// await seed_partners()
+// await seed_folders()
 await seed_customers()
-await seed_categories()
-await seed_materials()
+// await seed_categories()
+// await seed_materials()
 
 process.exit(0)
